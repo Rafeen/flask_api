e78c3bb6f089e9c046ead0913a1508953c13ca7f
Rafeen <rafeengunner@gmail.com>
2022-06-20 00:52:52 +0900

    upadted auth and validation

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, session, Response, jsonify, request
 import os
 from utils.utils import validate_username, validate_password
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.secret_key = "asdasd56465asd43123123446546^%*&%(&Asd7a987(&"
-
-
 
 
 @app.route('/signup', methods=['POST'])
@@ -21,57 +20,67 @@ def create_user():  # put application's code here
     user_id = json["user_id"]
     password = json["password"]
 
-    if(user_id == None or password ==None):
+    if (user_id == None or password == None):
         res = {
-                "message": "Account creation failed",
-                 "cause": "required user_id and password"
-            }
+            "message": "Account creation failed",
+            "cause": "required user_id and password"
+        }
         return Response(jsonify(res), status=400, mimetype='application/json')
-
 
     user_status = validate_username(user_id)
     password_status = validate_password(password)
 
-    if user_status["text_type"]:
+    if not user_status["text_type"]:
         res = {
-                "message": "Account creation failed",
-                "cause": "user_id is not alphanumeric"
-            }
+            "message": "Account creation failed",
+            "cause": "user_id is not alphanumeric"
+        }
         return jsonify(res), 400
 
-    if user_status["char_type"]:
+    if not user_status["char_type"]:
         res = {
-                "message": "Account creation failed",
-                "cause": "user_id is not half-width"
-            }
+            "message": "Account creation failed",
+            "cause": "user_id is not half-width"
+        }
         return jsonify(res), 400
 
-
-    if password_status["text_type"]:
+    if not user_status["length"]:
         res = {
-                "message": "Account creation failed",
-                "cause": "password is not alphanumeric"
-            }
+            "message": "Account creation failed",
+            "cause": "user_id length is not between 6 to 20"
+        }
         return jsonify(res), 400
 
-    if password_status["char_type"]:
+    if not password_status["text_type"]:
         res = {
-                "message": "Account creation failed",
-                "cause": "password is not half-width"
-            }
+            "message": "Account creation failed",
+            "cause": "password is not alphanumeric"
+        }
         return jsonify(res), 400
 
+    if not password_status["char_type"]:
+        res = {
+            "message": "Account creation failed",
+            "cause": "password is not half-width"
+        }
+        return jsonify(res), 400
+
+    if not password_status["length"]:
+        res = {
+            "message": "Account creation failed",
+            "cause": "password length is not between 8 to 20"
+        }
+        return jsonify(res), 400
 
     if session.get(user_id) != None:
         res = {
-                "message": "Account creation failed",
-                "cause": "already same user_id is used"
-            }
+            "message": "Account creation failed",
+            "cause": "already same user_id is used"
+        }
         return jsonify(res), 400
 
-
-    data[user_id] = user_id
-    data[password] = password
+    data['user_id'] = user_id
+    data['password'] = password
     session[user_id] = data
 
     return jsonify(data), 200
@@ -79,19 +88,30 @@ def create_user():  # put application's code here
 
 @app.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):  # put application's code here
+
+    if not "authorization" in request.headers:
+        return jsonify({"message": "Authentication failed for no auth header"}), 401
+
+    if "username" not in request.authorization or "password" not in request.authorization:
+        return jsonify({"message": "Authentication failed"}), 401
+
+    if request.authorization["username"] not in session:
+        return jsonify({"message": "Authentication failed  for invalid username"}), 401
+
     if request.authorization["username"] in session:
-        if session.get(request.authorization["username"])["password"] != request.authorization["password"]:
-            return jsonify({"message": "Authentication failed"}), 401
+        password = session.get(request.authorization["username"])["password"]
+        if password != request.authorization["password"]:
+            return jsonify({"message": "Authentication failed for invalid password"}), 401
 
     if session.get(user_id) == None:
         return jsonify({"message": "no user found"}), 404
     res = {
-        "message":"User Details by user_id",
-        "user":{
+        "message": "User Details by user_id",
+        "user": {
             "user_id": session.get(user_id)["user_id"],
-            "nickname":session.get(user_id)["nickname"],
-            "comment":session.get(user_id)["comment"],
-    }
+            "nickname": session.get(user_id)["nickname"],
+            "comment": session.get(user_id)["comment"],
+        }
     }
 
     return jsonify(res), 200
@@ -99,20 +119,28 @@ def get_user(user_id):  # put application's code here
 
 @app.route('/users/<user_id>', methods=['PATCH'])
 def update_user(user_id):  # put application's code here
+    if not "authorization" in request.headers:
+        return jsonify({"message": "Authentication failed for no auth header"}), 401
+
+    if "username" not in request.authorization or "password" not in request.authorization:
+        return jsonify({"message": "Authentication failed for invalid username or password"}), 401
 
     if request.authorization["username"] in session:
-        if session.get(request.authorization["username"])["password"] != request.authorization["password"]:
+        password = session.get(request.authorization["username"])["password"]
+        if password != request.authorization["password"]:
             return jsonify({"message": "Authentication failed"}), 401
 
     if session.get(user_id) == None:
         return jsonify({"message": "no user found"}), 404
 
     data = session.get(user_id)
-    data["nickname"] = request.json['nickname']
-    data["comment"] = request.json['comment']
+    if "nickname" in request.json:
+        data["nickname"] = request.json['nickname']
+    if "comment" in request.json:
+        data["comment"] = request.json['comment']
     session[user_id] = data
     res = {
-        "message":"update user by user id",
+        "message": "updated user by user id",
 
         "recipe": {
             "nickname": session.get(user_id)["nickname"],
@@ -125,8 +153,15 @@ def update_user(user_id):  # put application's code here
 
 @app.route('/close', methods=['POST'])
 def delete_user():  # put application's code here
+    if not "authorization" in request.headers:
+        return jsonify({"message": "Authentication failed"}), 401
+
+    if "username" not in request.authorization or "password" not in request.authorization:
+        return jsonify({"message": "Authentication failed"}), 401
+
     if request.authorization["username"] in session:
-        if session.get(request.authorization["username"])["password"] != request.authorization["password"]:
+        password = session.get(request.authorization["username"])["password"]
+        if password != request.authorization["password"]:
             return jsonify({"message": "Authentication failed"}), 401
 
     session.pop(request.authorization["username"])
--- utils/utils.py
+++ utils/utils.py
@@ -42,4 +42,3 @@ def validate_username(username):
 
 
 
-
